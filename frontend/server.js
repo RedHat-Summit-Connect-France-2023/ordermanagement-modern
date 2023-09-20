303e1cb8c998dee0c806d41d9c2ab5f5c5c6d1d0
mouachan <mouachan@redhat.com>
2023-09-20 17:21:09 +0200

    update coolstore.json

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 const app = express();
 const http = require('http');
 const path = require('path');
-const port = process.env.PORT;
+const port = process.env.PORT || 8085;
 
 app.use(express.static(__dirname + '/app'));
 
