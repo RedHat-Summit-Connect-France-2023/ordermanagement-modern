79568314a136f3c40f9e406c43fbc25659a3f3d1
gestrem <gestrem@redhat.com>
2023-10-02 15:28:21 +0200

    Update products.sql

--- inventory/src/main/resources/products.sql
+++ inventory/src/main/resources/products.sql
@@ -1,10 +1,8 @@
 
-insert into products (itemId, link, location, quantity, price,name,description, category) values (329299, '/imgs/Vortex_Tumbler.jpg', 'Raleigh', 5, 100,'16 oz. Vortex Tumbler','Vortex Tumbler','bottle');
-insert into products (itemId, link, location, quantity, price,name,description, category) values (329199, '/imgs/Atari_2600_Joystick.jpg', 'Raleigh', 512, 200,'Atari 2600 Joystick', 'Joystick','Object');
-insert into products (itemId, link, location, quantity, price,name,description, category) values (165613, '/imgs/Forge_Laptop_Sticker.jpg', 'Raleigh', 256, 300,'Forge Laptop Sticker','Forge laptop','Laptop');
-insert into products (itemId, link, location, quantity, price,name,description, category) values (165614, '/imgs/Lytro_Camera.jpg', 'Raleigh', 54, 400,'Lytro Camera','Camera Lytro','Camera');
-insert into products (itemId, link, location, quantity, price,name,description, category) values (165954, '/imgs/Oculus_Rift.jpg', 'Raleigh', 87, 500, 'Oculus Rift', 'Oculus Rift','Object');
-insert into products (itemId, link, location, quantity, price,name,description, category) values (444434, '/imgs/Ogio_Caliber_Polo.jpg', 'Raleigh', 443, 578,'Ogio Caliber Polo','Ogio','shirt');
-insert into products (itemId, link, location, quantity, price,name,description, category) values (444435, '/imgs/Pebble_Smart_Watch.jpg', 'Raleigh', 600, 2,'Pebble Smart Watch','Pebble Smart Watch','watch');
-insert into products (itemId, link, location, quantity, price,name,description, category) values (444437, '/imgs/Solid_Performance_Polo.jpg', 'Tokyo', 300, 5,'Solid Performance Polo','Solid Performance Polo','shirt');
+insert into products (itemId, link, location, quantity, price,name,description, category) values (329299, 'https://minio-object-detection.apps.summitconnect.sandbox2218.opentlc.com/images/YW5vbnltb3Vz/a041e121-6a00-4b87-8c34-caf27919005d.jpg', 'Raleigh', 1, 10,'Pantalon','beige','chino');
+insert into products (itemId, link, location, quantity, price,name,description, category) values (329199, 'https://minio-object-detection.apps.summitconnect.sandbox2218.opentlc.com/images/YW5vbnltb3Vz/32fc47a1-a516-4647-9983-c3eea06d1443.jpg', 'Raleigh', 1, 0,'T-Shirt', 'vert','chemise');
+insert into products (itemId, link, location, quantity, price,name,description, category) values (165613, 'https://minio-object-detection.apps.summitconnect.sandbox2218.opentlc.com/images/YW5vbnltb3Vz/8569f860-2007-4c4f-ad23-2a0d8a34b59d.jpg', 'Raleigh', 1, 5,'Veste Jean','bleu','Laptop');
+insert into products (itemId, link, location, quantity, price,name,description, category) values (165614, 'https://minio-object-detection.apps.summitconnect.sandbox2218.opentlc.com/images/YW5vbnltb3Vz/93331895-2f41-4d4f-a275-2ed0af221c94.jpg', 'Raleigh', 1, 20,'Polo','bleu','chemise');
+insert into products (itemId, link, location, quantity, price,name,description, category) values (165954, 'https://minio-object-detection.apps.summitconnect.sandbox2218.opentlc.com/images/YW5vbnltb3Vz/d42c9a20-9698-4b72-9db8-2297f2392e10.jpg', 'Raleigh', 1, 50, 'T-Shirt', 'Noir','chemise');
+
 
